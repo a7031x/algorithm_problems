dea9637718072126b3756aac0b737cedfd9b8d66
easton.hou <easton.hou@ringcentral.com>
2016-09-25 22:23:14 +0800

    optimized algorithm

--- problems/gridland_provinces.hpp
+++ problems/gridland_provinces.hpp
@@ -1,53 +1,77 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <unordered_set>
-#include <set>
+#include <random>
 
 namespace gridland_provinces
 {
-	inline bool valid_move(size_t row, size_t column, const std::vector<char> grid[2])
+	const size_t max_column_size = 600;
+	const size_t max_string_size = max_column_size * 2;
+
+	inline void initial_hash_table(int64_t hash_table[max_string_size][128])
 	{
-		if (0 == grid[row][column] || grid[row][column - 1] && grid[row][column + 1])
-			return false;
-		else
-			return true;
+		std::default_random_engine generator;
+		std::uniform_int_distribution<int64_t> distribution;
+
+		for (size_t k = 0; k < max_string_size; ++k)
+			for (size_t j = 0; j < 128; ++j)
+			{
+				hash_table[k][j] = distribution(generator);
+			}
 	}
 
-	inline void solve(
-		size_t start_row, size_t start_column,
-		std::vector<char> grid[2],
-		std::vector<char>& text,
-		std::set<std::string>& hash,
-		size_t index)
+	inline int64_t string_hash(int64_t hash_table[][128], const std::string& text, size_t offset = 0)
 	{
-		char c = grid[start_row][start_column];
-		grid[start_row][start_column] = 0;
-		text[index++] = c;
-		if (grid[0].size() * 2 - 4 > index)
-		{
-			if (valid_move(1 - start_row, start_column, grid))
-				solve(1 - start_row, start_column, grid, text, hash, index);
-			if (grid[start_row][start_column - 1])
-				solve(start_row, start_column - 1, grid, text, hash, index);
-			if (grid[start_row][start_column + 1])
-				solve(start_row, start_column + 1, grid, text, hash, index);
-		}
-		else
-		{
-			hash.insert(text.data());
-		}
-		grid[start_row][start_column] = c;
-		text[--index] = 0;
+		int64_t sum = 0;
+		size_t k = offset;
+		for (auto c : text)
+			sum ^= hash_table[k++][c];
+		return sum;
 	}
 
-	inline size_t solve(const std::string s[2])
+	class warmup_hash_t
 	{
-		std::set<std::string> rset;
+	public:
+		warmup_hash_t(int64_t hash_table[max_string_size][128], const std::string s[2])
+		{
+			std::string sr[2] =
+			{
+				std::string(s[0].crbegin(), s[0].crend()),
+				std::string(s[1].crbegin(), s[1].crend())
+			};
+
+			size_t length = s[0].size();
+
+			for (size_t row = 0; row < 2; ++row)
+			for (size_t column = 0; column < length; ++column)
+			{
+				std::string left = sr[row].substr(length - column - 1, column + 1) + s[1 - row].substr(0, column + 1);
+				std::string right = s[row].substr(column, length - column) + sr[1 - row].substr(0, length - column);
+				_cache[row][0][0][column] = string_hash(hash_table, left, 0);
+				_cache[row][0][1][column] = string_hash(hash_table, left, length * 2 - left.size());
+				_cache[row][1][0][column] = string_hash(hash_table, right, 0);
+				_cache[row][1][1][column] = string_hash(hash_table, right, length * 2 - right.size());
+			}
+		}
+
+		int64_t get_hash(size_t column, size_t row, bool from_end, bool right)
+		{
+			return _cache[row][right][from_end][column];
+		}
+	private:
+		int64_t _cache[2][2][2][max_column_size];
+	};
+
+	inline size_t solve(int64_t hash_table[][128], const std::string s[2])
+	{
+		std::unordered_set<int64_t> rset;
 		std::string sr[2] = { std::string(s[0].crbegin(), s[0].crend()), std::string(s[1].crbegin(), s[1].crend()) };
 		std::string curl[2];
+		warmup_hash_t cache(hash_table, s);
+
 		size_t length = s[0].size();
+
 		for (size_t k = 0; k < length; ++k)
 		{
 			curl[0] += s[k % 2][k];
@@ -60,76 +84,61 @@ namespace gridland_provinces
 		{
 			for (size_t r0 = 0; r0 < 2; ++r0)
 			{
-				std::string left, right;
-				left = std::string(s[r0].crbegin() + (length - 1 - c0), s[r0].crend());
-				left += s[1 - r0].substr(0, c0 + 1);
+				auto left_hash = cache.get_hash(c0 , r0, false, false);
+				auto right_hash = cache.get_hash(c0, r0, false, true);
 
-				right = s[r0].substr(c0, length - c0);
-				right += std::string(s[1 - r0].crbegin(), s[1 - r0].crbegin() + (length - c0));
+				if (length - 1 == c0)
+					rset.insert(left_hash);
 
-				if (left.size() == length * 2)
-					rset.insert(left);
-
-				if (right.size() == length * 2)
-					rset.insert(right);
+				if (0 == c0)
+					rset.insert(right_hash);
 
 				auto curl_ref = curl[c0 % 2 ? (1 - r0) : r0];
-				std::string left_curl;
+				int64_t curl_hash = 0;
+				size_t curl_index = (c0 + 1) * 2;
+
 				for (size_t c1 = c0 + 1; c1 < length; ++c1)
 				{
 					size_t r1 = (c1 - c0) % 2 ? (1 - r0) : r0;
-					std::string forward = s[r1].substr(c1, length - c1);
-					std::string backward(sr[1 - r1].cbegin(), sr[1 - r1].cbegin() + (length - c1));
-					rset.insert(left + left_curl + forward + backward);
-					left_curl.push_back(curl_ref[c1 * 2]);
-					left_curl.push_back(curl_ref[c1 * 2 + 1]);
+					auto hash = left_hash ^ curl_hash ^ cache.get_hash(c1, r1, true, true);
+					rset.insert(hash);
+					curl_hash ^= hash_table[curl_index++][curl_ref[c1 * 2]];
+					curl_hash ^= hash_table[curl_index++][curl_ref[c1 * 2 + 1]];
 				}
 
 				curl_ref = curl[c0 % 2 ? r0 : (1 - r0)];
-				std::string right_curl(
+
+				curl_hash = string_hash(
+					hash_table,
+					std::string(
 						curl_ref.crbegin() + (length - c0) * 2,
-						curl_ref.crbegin() + length * 2);
+						curl_ref.crbegin() + length * 2),
+					(length - c0) * 2);
 
 				for (size_t c1 = 0; c1 < c0; ++c1)
 				{
-					right_curl.resize(right_curl.size() - 2);
+					curl_hash ^= hash_table[--curl_index][curl_ref[c1 * 2]];
+					curl_hash ^= hash_table[--curl_index][curl_ref[c1 * 2 + 1]];
 					size_t r1 = (c1 - c0) % 2 ? (1 - r0) : r0;
-					std::string forward(sr[r1].cbegin() + (length - c1 - 1), sr[r1].cend());
-					std::string backward = s[1 - r1].substr(0, c1 + 1);
-					rset.insert(right + right_curl + forward + backward);
+					auto hash = right_hash ^ curl_hash ^ cache.get_hash(c1, r1, true, false);
+					rset.insert(hash);
 				}
 			}
 		}
 		return rset.size();
 	}
 
-	inline int64_t solve(size_t columns, const std::string& s0, const std::string& s1)
+	inline int64_t solve(int64_t hash_table[][128], const std::string& s0, const std::string& s1)
 	{
-	/*	std::vector<char> grid[2];
-		grid[0].push_back(0);
-		grid[1].push_back(0);
-		for (size_t k = 0; k < columns; ++k)
-		{
-			grid[0].push_back(s0[k]);
-			grid[1].push_back(s1[k]);
-		}
-		grid[0].push_back(0);
-		grid[1].push_back(0);
-
-		std::set<std::string> hash;
-		std::vector<char> text(columns * 2 + 1, 0);
-		for (size_t k = 1; k <= columns; ++k)
-		{
-			solve(0, k, grid, text, hash, 0);
-			solve(1, k, grid, text, hash, 0);
-		}
-		*/
 		std::string s[2] = { s0, s1 };
-		return solve(s);
+		return solve(hash_table, s);
 	}
 
 	inline void solve()
 	{
+		static int64_t hash_table[max_string_size][128];
+		initial_hash_table(hash_table);
+
 		int t;
 		std::cin >> t;
 		while (t--)
@@ -138,7 +147,7 @@ namespace gridland_provinces
 			std::cin >> columns;
 			std::string s0, s1;
 			std::cin >> s0 >> s1;
-			std::cout << solve(columns, s0, s1) << std::endl;
+			std::cout << solve(hash_table, s0, s1) << std::endl;
 		}
 	}
 }
