73390e1e9579e64a3c8393b59b5ab77043119d96
easton.hou <easton.hou@ringcentral.com>
2016-11-25 23:54:11 +0800

    merge mod into number theory

--- algorithm_lib/number_theory.hpp
+++ algorithm_lib/number_theory.hpp
@@ -2,9 +2,13 @@
 #include <vector>
 #include <math.h>
 #include <unordered_set>
+#include <unordered_map>
+#include <memory.h>
 
 namespace algorithm_lib
 {
+	const int m1e9n7 = 1000000007;
+
 	class number_theory_t
 	{
 	public:
@@ -119,7 +123,15 @@ namespace algorithm_lib
 		}
 
 		template<typename T>
-		static std::vector<T> distinct_prime_factors(T n)
+		static bool is_prime(const T& n)
+		{
+			if (1 >= n)
+				return false;
+			return 1 == prime_factors(n).size();
+		}
+
+		template<typename T>
+		static std::vector<T> distinct_prime_factors(const T& n)
 		{
 			auto r0 = prime_factors(n);
 			std::unordered_set<T> r(r0.begin(), r0.end());
@@ -162,7 +174,7 @@ namespace algorithm_lib
 			for (auto factor : factors)
 			{
 				factor = (p - 1) / factor;
-				if (power_mod(candidate, factor, p) == 1)
+				if (power_mod<int64_t>(candidate, factor, p) == 1)
 					return false;
 			}
 			return true;
@@ -185,6 +197,156 @@ namespace algorithm_lib
 		{
 			auto factors = distinct_prime_factors(p - 1);
 			return find_primitive_root(p, factors);
+		}
+
+		template<typename T = int64_t, typename U = int64_t>
+		static T mod(const T& v, const U& m = m1e9n7)
+		{
+			if (v >= 0)
+				return v % m;
+			else
+				return (v % m + m) % m;
+		}
+
+		template<typename T, typename U>
+		static T smod(const T& v, const U& m = m1e9n7)
+		{
+			return v % m;
+		}
+
+		template<size_t A, size_t B, size_t C>
+		static void matrix_multiply(int64_t a[A][B], int64_t b[B][C], int64_t c[A][C], int64_t m = m1e9n7)
+		{
+			for (size_t k0 = 0; k0 < A; ++k0)
+				for (size_t k2 = 0; k2 < C; ++k2)
+				{
+					int64_t sum = 0;
+					for (size_t k1 = 0; k1 < B; ++k1)
+						sum = smod(sum + a[k0][k1] * b[k1][k2], m);
+					c[k0][k2] = sum;
+				}
+		}
+
+		template<size_t A, size_t B, size_t C>
+		static void matrix_multiply_inplace(int64_t a[A][B], int64_t b[B][C], int64_t c[A][C], int64_t m = m1e9n7)
+		{
+			int64_t c0[A][C];
+			matrix_multiply<A, B, C>(a, b, c0, m);
+			memcpy(c, c0, sizeof(c0));
+		}
+
+		template<size_t N, int64_t M = m1e9n7>
+		static void matrix_power_mod(int64_t m[N][N], int64_t p)
+		{
+			if (0 == p)
+			{
+				for (size_t r = 0; r < N; ++r)
+					for (size_t c = 0; c < N; ++c)
+						m[r][c] = r == c;
+				return;
+			}
+			if (1 == p)
+				return;
+			if (p & 1)
+			{
+				int64_t m1[N][N];
+				memcpy(m1, m, sizeof(m1));
+				matrix_power_mod<N>(m, p / 2);
+				matrix_multiply_inplace<N, N, N>(m, m, m);
+				matrix_multiply_inplace<N, N, N>(m, m1, m);
+			}
+			else
+			{
+				matrix_power_mod<N>(m, p / 2);
+				matrix_multiply_inplace<N, N, N>(m, m, m);
+			}
+		}
+
+		template<typename T>
+		static T modular_multiplicative_inverse(const T& a, const T& m)
+		{
+			int64_t mat[2][2] = { 1, 0, 0, 1 };
+			T b = m;
+			T va = a;
+			do {
+				auto r = va % b;
+				auto q = va / b;
+				va = b;
+				b = r;
+				int64_t mat0[2][2] = { 0, 1, 1, -q };
+				matrix_multiply_inplace<2, 2, 2>(mat0, mat, mat, std::numeric_limits<int64_t>::max());
+				if (1 == r)
+					break;
+			} while (true);
+			return (T)mod(mat[1][0], m);
+		}
+
+		template<typename T>
+		static T modular_multiplicative_inverse(const T& a, const T& b, const T& m)
+		{
+			auto g = gcd(a, m);
+			if (b / g * g != b)
+				return -1;
+			auto r = modular_multiplicative_inverse(a / g, m / g);
+			r = mod(r * b / g, m / g);
+			return r;
+		}
+
+		template<typename T>
+		static T power_mod(const T& a, const T& p, const T& m = m1e9n7)
+		{
+			if (0 > p)
+				return modular_multiplicative_inverse(power_mod(a, -p, m), m);
+
+			if (1 == p)
+				return a % m;
+			if (0 == p)
+				return 1;
+
+			T r = power_mod(a, p / 2, m);
+			r = r * r % m;
+			if (p & 1)
+				return r * a % m;
+			else
+				return r;
+		}
+
+		template<typename T>
+		static std::unordered_map<T, size_t> dlog_precalc(const T& a, const T& n)
+		{
+			T sqrt_n = (T)ceil(sqrt(n));
+			std::unordered_map<T, size_t> vmap;
+			T s0 = 1;
+			vmap[1] = 0;
+			for (size_t k = 1; k < (size_t)sqrt_n; ++k)
+			{
+				s0 = (T)mod((int64_t)s0 * a, n);
+				vmap[s0] = k;
+			}
+			return vmap;
+		}
+
+		template<typename T>
+		static T dlog(const T& a, const T& n, const T& e, const std::unordered_map<T, size_t>& precalc)
+		{
+			T sqrt_n = (T)ceil(sqrt(n));
+			auto ak = power_mod<int64_t>(a, -sqrt_n, n);
+			auto r = e;
+			for (T k = sqrt_n; k < n; k += sqrt_n)
+			{
+				r = (T)mod(r * ak, n);
+				auto it = precalc.find(r);
+				if (precalc.end() != it)
+					return mod(it->second + k, n - 1);
+			}
+
+			return -1;
+		}
+
+		template<typename T>
+		static T dlog(const T& a, const T& n, const T& e)
+		{
+			return dlog(a, n, e, dlog_precalc(a, n));
 		}
 	};
 }
